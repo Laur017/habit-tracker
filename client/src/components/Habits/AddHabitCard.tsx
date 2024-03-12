f00ac390a69bc8ede13ba46d33c192ec0191d4df
Laurentiu Popa <popa.laur.01@gmail.com>
2024-03-12 15:40:07 +0200

    Adding habits in a list

--- client/src/components/Habits/AddHabitCard.tsx
+++ client/src/components/Habits/AddHabitCard.tsx
@@ -1,19 +1,22 @@
+import { useState } from "react"
 
 export default function AddHabitCard({toggleAddHabit}) {
+    const [selectedColor, setSelectedColor] = useState<number>(1)
+    const [habit, setHabit] = useState<string>()
   return (
     <div className="add-habit-card flex flex-col">
         <h3>Let's add a new habit !</h3>
-        <input type="text" placeholder="habit" />
+        <input type="text" placeholder="habit" value={habit} onChange={(e) => setHabit(e.target.value)}/>
         <div className="colors">
-            <span className="selected-color"></span>
-            <span className=""></span>
-            <span className=""></span>
-            <span className=""></span>
-            <span className=""></span>
+            <span className={`${selectedColor === 1 ? 'selected-color' : ''}`} onClick={() => setSelectedColor(1)}></span>
+            <span className={`${selectedColor === 2 ? 'selected-color' : ''}`} onClick={() => setSelectedColor(2)}></span>
+            <span className={`${selectedColor === 3 ? 'selected-color' : ''}`} onClick={() => setSelectedColor(3)}></span>
+            <span className={`${selectedColor === 4 ? 'selected-color' : ''}`} onClick={() => setSelectedColor(4)}></span>
+            <span className={`${selectedColor === 5 ? 'selected-color' : ''}`} onClick={() => setSelectedColor(5)}></span>
         </div>
         <div className="add-card-btns">
-            <button onClick={toggleAddHabit}>Cancel</button>
-            <button>Add</button>
+            <button onClick={() => toggleAddHabit(1,"","")}>Cancel</button>
+            <button onClick={() => toggleAddHabit(2,habit,selectedColor)}>Add</button>
         </div>
     </div>
   )
--- client/src/components/Habits/Habits.tsx
+++ client/src/components/Habits/Habits.tsx
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Bin from '../../assets/icons/bin.png'
+import Edit from '../../assets/icons/edit.png'
 import '../Dashboard/Dashboard.css'
 import Left from "../Dashboard/Left";
 import AddHabitCard from './AddHabitCard';
@@ -6,11 +8,23 @@ import { useParams } from "react-router-dom";
 
 export default function Habits() {
     const { name} = useParams()
-    const [addBtn, setAddBtn] = useState<boolean>(true)
+    const [addBtn, setAddBtn] = useState<boolean>(false)
+    const [habits, setHabits] = useState<{habit:string, color:number}[]>([])
 
-    const toggleAddHabit = () => {
-      setAddBtn(false)
+    const toggleAddHabit = (t:number,hab:string,col:number) => {
+      if(t === 1)
+        setAddBtn(false)
+      if(t === 2){
+        const aux = habits;
+        aux.push({habit:hab,color:col})
+        setHabits(aux)
+        setAddBtn(false)
+      }
     }
+
+    useEffect(() => {
+      console.log(habits)
+    },[habits])
 
   return (
     <div className="h-[100%] w-[100%]">
@@ -22,6 +36,30 @@ export default function Habits() {
                 <h2>Habits List</h2>
                 <button className='add-hab-btn' onClick={() => setAddBtn(true)}>+ Add Habit</button>
               </div>
+              {habits && 
+              <div className='bottom-hab-div mt-[5%] flex flex-col gap-[1rem]'>
+                {habits.map(i => 
+                <div className='habits-list-div flex items-center'>
+                  <div className='flex items-center gap-[1rem] h-[100%]'>
+                  <span 
+                  className=
+                    {
+                      `${i.color === 1 ? 'first-col' 
+                      : i.color === 2 ? 'second-col' 
+                      : i.color === 3 ? 'third-col'
+                      : i.color === 4 ? 'fourth-col'
+                    : 'fifth-col'}`
+                    }>
+                  </span>
+                  {i.habit}
+                  </div>
+                  <div className='flex items-center gap-[1rem]'>
+                    <img src={Edit} className='w-[1.5rem] cursor-pointer' />
+                    <img src={Bin} className='w-[1.5rem] cursor-pointer' />
+                  </div>
+                </div>)}
+              </div>  
+              }
             </div>
         </div>
         }
