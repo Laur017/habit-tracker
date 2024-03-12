3a33b569c175f95c63989cd00ff19a401f6b8cae
Laurentiu Popa <popa.laur.01@gmail.com>
2024-03-12 11:40:41 +0200

    Getting the today date for the dashboard

--- client/src/components/Dashboard/Mid.tsx
+++ client/src/components/Dashboard/Mid.tsx
@@ -1,5 +1,19 @@
+import './Dashboard.css'
+
 export default function Mid() {
+  const today = new Date()
+  const month = today.toLocaleString('default', { month: 'long' })
+  const date = today. getDate()
+
   return (
-    <div className="w-[60%]">Mid</div>
+    <div className="w-[60%] p-[5%]">
+      <div className="top-dash flex items-center">
+        <span>
+          <h2>Today</h2>
+          <h4>{month} {date}</h4>
+        </span>
+        <button className='add-hab-btn'>+ Add Habit</button>
+      </div>
+    </div>
   )
 }
